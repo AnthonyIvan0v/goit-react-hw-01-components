0a1a39c89339ae961bcf724702e5cab388e84d83
AnthonyIvan0v <cat.sledgehammer@gmail.com>
2023-01-18 19:50:07 +0200

    додає компонент в Арр та прередає пропси

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,25 +1,21 @@
 import Profile from "./Profile/Profile";
-import user from '../data/user/user.json';
+import user from "../data/user/user.json";
+import Statistics from "./Statistics/Statistics";
+import data from "../data/statistics/data.json"
 
 export const App = () => {
   return (
-    // <div
-    //   style={{
-    //     height: '100vh',
-    //     display: 'flex',
-    //     justifyContent: 'center',
-    //     alignItems: 'center',
-    //     fontSize: 40,
-    //     color: '#010101'
-    //   }}
-    // >
+    <>
       <Profile
         username={user.username}
         tag={user.tag}
         location={user.location}
         avatar={user.avatar}
-        stats={user.stats}/>
-      // React homework template
-    // </div>
+        stats={user.stats}/>    
+      
+      <Statistics
+        title="Upload stats"
+        stats={data} />
+      </>
   );
 };
